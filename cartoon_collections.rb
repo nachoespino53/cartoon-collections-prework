751205ef1f3649351bcdf1d5fd00fce67d10dc0f
Ignacio R Espino <irespino@capozios.com>
2018-08-29 00:11:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   for i in 0..2 do
-    if array.any?(cheese_types[i])
+    if array.include(cheese_types[i])
       return cheese_types[i]
     end
   end
